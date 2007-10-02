224862f814f443f61b853d76e43adbde7f11c122
Stefan Siegl <stesie@gnubox.metafnord.de>
2007-10-02 23:30:12 +0200

    in6.h: define IPV6_MTU_DISCOVER values.

--- glue-include/linux/in6.h
+++ glue-include/linux/in6.h
@@ -1,1 +1,7 @@
 #include <netinet/in.h>
+
+/* IPV6_MTU_DISCOVER values */
+#define IPV6_PMTUDISC_DONT              0
+#define IPV6_PMTUDISC_WANT              1
+#define IPV6_PMTUDISC_DO                2
+
